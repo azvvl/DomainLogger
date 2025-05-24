ab859c901ba0fb1c95e20319c5e9a010296fbe52
azvvl <sergiorobles876@outlook.com>
2025-05-24 18:20:23 -0500

    source code

---
+++ source.js
@@ -0,0 +1,10 @@
+if (window.location.href !== "https://freedns.afraid.org/domain/registry/") {
+    console.log("You are not on the correct page");
+} else {
+    const domains = [];
+    document.querySelectorAll('a[href^="/subdomain/edit.php?edit_domain_id="]').forEach(a => {
+        const domain = a.textContent.trim();
+        if (domain) domains.push(domain);
+    });
+    console.log(domains.join('\n'));
+}
